b1254c4dec0fe3071af1953124a79f1dae5caa69
treatme030 <treatme8192@gmail.com>
2021-10-28 18:24:57 +0900

    글로벌 스타일 설정

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const PostListPage = () => {
     return (
-        <div>포스트 목록</div>
+        <div>
+            <Button>버튼</Button>
+        </div>
     );
 };
 
