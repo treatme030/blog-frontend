996b95dd39094e7a75ced1c07a448923afa0d32f
treatme030 <treatme8192@gmail.com>
2021-10-30 00:29:09 +0900

    로그아웃 구현

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,7 +1,7 @@
 import { createAction, handleActions } from "redux-actions";
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, { createRequestActionTypes } from "../lib/createRequestSaga";
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, call } from 'redux-saga/effects';
 
 //새로고침 이후 임시 로그인 처리
 const TEMP_SET_USER = 'user/TEMP_SET_USER';
@@ -9,9 +9,11 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER';
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
     'user/CHECK',
 )
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -23,9 +25,19 @@ function checkFailureSaga() {
     }
 }
 
+function* logoutSaga() {
+    try {
+        yield call(authAPI.logout);
+        localStorage.removeItem('user');
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 export function* userSaga() {
     yield takeLatest(CHECK, checkSaga);
     yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+    yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -49,6 +61,10 @@ const user = handleActions(
             user: null,
             checkError: error,
         }),
+        [LOGOUT]: state => ({
+            ...state,
+            user: null,
+        })
     },
     initialState,
 )
