65102bedd815eb0f6eeff65b3f4fd670b50f01c2
treatme030 <treatme8192@gmail.com>
2021-10-29 14:15:03 +0900

    로그인 구현

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import AuthForm from '../../components/auth/AuthForm';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
+import { check } from '../../modules/user';
+
 
 const LoginForm = () => {
+    const history = useHistory();
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.login
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+        form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }))
 
     const onChange = e => {
@@ -22,12 +29,32 @@ const LoginForm = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        const { username, password } = form;
+        dispatch(login({ username, password }));
     }
 
     //컴포넌트 처음 렌더링될 때 폼 초기화
     useEffect(() => {
         dispatch(initializeForm('login'))
     }, [dispatch])
+
+    useEffect(() => {
+        if (authError) {
+            console.log('error')
+            console.log(authError)
+            return;
+        }
+        if (auth) {
+            console.log('success')
+            dispatch(check())
+        }
+    }, [auth, authError, dispatch])
+
+    useEffect(() => {
+        if (user) {
+            history.push('/')
+        }
+    }, [history, user])
 
     return (
         <AuthForm
