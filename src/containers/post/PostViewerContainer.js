e100752d9aaac507c1e05842903069cdedf04fe0
treatme030 <treatme8192@gmail.com>
2021-11-01 12:05:46 +0900

    포스트 읽기 페이지 구현, API 연동

---
+++ src/containers/post/PostViewerContainer.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import PostViewer from '../../components/post/PostViewer';
+import { readPost, unloadPost } from '../../modules/post';
+
+const PostViewerContainer = ({ match }) => {
+    const { postId } = match.params;
+    const dispatch = useDispatch()
+    const { post, error, loading } = useSelector(({ post, loading }) => ({
+        post: post.post,
+        error: post.error,
+        loading: loading['post/READ_POST'],
+    }));
+
+    useEffect(() => {
+        dispatch(readPost(postId));
+        // 언마운트될 때 리덕스에서 포스트 데이터 없애기
+        return () => {
+            dispatch(unloadPost());
+        }
+    }, [dispatch, postId])
+
+    return (
+        <PostViewer post={post} loading={loading} error={error} />
+    );
+};
+
+export default withRouter(PostViewerContainer);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,19 +1,21 @@
 import { all } from "redux-saga/effects";
 import { combineReducers } from "redux";
-import auth, { authSaga } from './auth';
-import user, { userSaga } from './user';
+import auth, { authSaga } from "./auth";
+import user, { userSaga } from "./user";
 import loading from "./loading";
 import write, { writeSaga } from "./write";
+import post, { postSaga } from "./post";
 
 const rootReducer = combineReducers({
     auth,
     user,
     loading,
     write,
+    post,
 })
 
 export function* rootSaga() {
-    yield all([authSaga(), userSaga(), writeSaga()])
+    yield all([authSaga(), userSaga(), writeSaga(), postSaga()])
 }
 
 export default rootReducer;
